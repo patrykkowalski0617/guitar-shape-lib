0dbe4ba59d688ea689303126b7b30674205167f5
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-09 20:07:39 +0100

    MUSIC_KEYS order by one semitone

--- src/utils/musicKeys/musicKeys.ts
+++ src/utils/musicKeys/musicKeys.ts
@@ -9,18 +9,17 @@ export interface MusicKeyInfo {
 
 export const MUSIC_KEYS = {
   C: { firstNote: "C", isFlatKey: false, unifiedName: "C / Am" },
-  G: { firstNote: "G", isFlatKey: false, unifiedName: "G / Em" },
-  D: { firstNote: "D", isFlatKey: false, unifiedName: "D / Bm" },
-  A: { firstNote: "A", isFlatKey: false, unifiedName: "A / F#m" },
-  E: { firstNote: "E", isFlatKey: false, unifiedName: "E / C#m" },
-  B: { firstNote: "B", isFlatKey: false, unifiedName: "B / G#m" },
-  Fsharp: { firstNote: "F#", isFlatKey: false, unifiedName: "F# / D#m" },
-  F: { firstNote: "F", isFlatKey: true, unifiedName: "F / Dm" },
-  Bb: { firstNote: "Bb", isFlatKey: true, unifiedName: "Bb / Gm" },
-  Eb: { firstNote: "Eb", isFlatKey: true, unifiedName: "Eb / Cm" },
-  Ab: { firstNote: "Ab", isFlatKey: true, unifiedName: "Ab / Fm" },
   Db: { firstNote: "Db", isFlatKey: true, unifiedName: "Db / Bbm" },
-  Gb: { firstNote: "Gb", isFlatKey: true, unifiedName: "Gb / Ebm" },
+  D: { firstNote: "D", isFlatKey: false, unifiedName: "D / Bm" },
+  Eb: { firstNote: "Eb", isFlatKey: true, unifiedName: "Eb / Cm" },
+  E: { firstNote: "E", isFlatKey: false, unifiedName: "E / C#m" },
+  F: { firstNote: "F", isFlatKey: true, unifiedName: "F / Dm" },
+  Fsharp: { firstNote: "F#", isFlatKey: false, unifiedName: "F# / D#m" },
+  G: { firstNote: "G", isFlatKey: false, unifiedName: "G / Em" },
+  Ab: { firstNote: "Ab", isFlatKey: true, unifiedName: "Ab / Fm" },
+  A: { firstNote: "A", isFlatKey: false, unifiedName: "A / F#m" },
+  B: { firstNote: "B", isFlatKey: false, unifiedName: "B / G#m" },
+  Bb: { firstNote: "Bb", isFlatKey: true, unifiedName: "Bb / Gm" },
 } as const;
 
 export type MusicKeyId = keyof typeof MUSIC_KEYS;
