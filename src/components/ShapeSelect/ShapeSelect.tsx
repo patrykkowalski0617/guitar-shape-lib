06dbd3b34ac5a7f10968c66438096fa6cf7cf12f
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-15 21:02:56 +0100

    shapes refactor

--- src/components/ShapeSelect/ShapeSelect.tsx
+++ src/components/ShapeSelect/ShapeSelect.tsx
@@ -6,26 +6,25 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useControlsStore } from "@/store/useControlsStore";
-import { UNIFIED_MUSIC_KEYS, type MusicKeyId } from "@/utils";
 import { GroupWrapper, Label } from "../InputGroup/InputGroup";
 import { useSettingsStore } from "@/store/useSettingsStore";
 
 export default function ShapeSelect() {
-  const currentKeyId = useControlsStore((state) => state.currentKeyId);
   const isMajorMode = useControlsStore((state) => state.isMajorMode);
-  const setCurrentKey = useControlsStore((state) => state.setCurrentKey);
+  const currentKeyId = useControlsStore((state) => state.currentKeyId);
+  const currentRoleId = useControlsStore((state) => state.currentRoleId);
   const areDescriptiveLabels = useSettingsStore((state) => state.areDescriptiveLabels);
 
-  const options = Object.entries(UNIFIED_MUSIC_KEYS).map(([id, data]) => ({
-    id: id as MusicKeyId,
-    majorName: data.majorName,
-    relativeMinorName: data.relativeMinorName,
-  }));
+  if (currentRoleId) {
+    console.log(roles[currentRoleId]);
+  }
+
+  const options = ["option1"];
 
   return (
     <GroupWrapper>
       <Label>{areDescriptiveLabels ? "Set of notes" : "Shapes"}</Label>
-      <Select value={currentKeyId} onValueChange={(v) => setCurrentKey(v as MusicKeyId)}>
+      <Select value={currentKeyId} onValueChange={(v) => {}}>
         <SelectTrigger
           style={{ height: "40px" }}
           className="bg-muted/30 border-muted-foreground/20 focus:ring-0 focus:ring-offset-0 font-semibold"
@@ -34,12 +33,8 @@ export default function ShapeSelect() {
         </SelectTrigger>
         <SelectContent className="font-semibold">
           {options.map((opt) => (
-            <SelectItem key={opt.id} value={opt.id}>
-              <span className={isMajorMode ? "opacity-100" : "opacity-50"}>{opt.majorName}</span>
-              <span className="mx-1 opacity-50">/</span>
-              <span className={!isMajorMode ? "opacity-100" : "opacity-50"}>
-                {opt.relativeMinorName}
-              </span>
+            <SelectItem key={opt} value={opt}>
+              <span className={isMajorMode ? "opacity-100" : "opacity-50"}>{opt}</span>
             </SelectItem>
           ))}
         </SelectContent>
