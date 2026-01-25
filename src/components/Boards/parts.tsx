5429d07fbde0e51976a8758151c706f786f987df
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-25 20:15:01 +0100

    add mask for boards

--- src/components/Boards/parts.tsx
+++ src/components/Boards/parts.tsx
@@ -4,13 +4,20 @@ export const BoardWrapper = styled.div`
   min-width: 1200px;
   margin: auto;
   overflow: hidden;
-  padding: 0 5px;
+  padding: 0 10px;
 `;
 
 export const BoardScrollWrapper = styled.div`
   overflow-x: auto;
   position: relative;
   scrollbar-width: none;
+  mask-image: linear-gradient(
+    to right,
+    transparent,
+    var(--background) 15px,
+    var(--background) calc(100% - 15px),
+    transparent
+  );
   border-radius: var(--radius-lg);
   margin-left: -5px; //- compensation for VariantProgressDots
 `;
