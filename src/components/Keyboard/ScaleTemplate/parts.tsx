bffb7500c716c15eac88fca4c5531f451bc9ce0a
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-01-25 20:15:57 +0100

    layout refactor

--- src/components/Keyboard/ScaleTemplate/parts.tsx
+++ src/components/Keyboard/ScaleTemplate/parts.tsx
@@ -20,7 +20,7 @@ interface TemplateWrapperProps {
 }
 
 export const TemplateWrapper = styled.div<TemplateWrapperProps>`
-  height: 30px;
+  height: 27px;
   transform: ${({ $position, $numberOfKeys }) => {
     return `translateX(calc(
       (${$position} + (${KEY_PADDING} * ${LEFT_PADDING_FACTOR})) * ${KEY_WIDTH_CSS($numberOfKeys)}
