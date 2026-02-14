0ea92072ad798ce974079acef9c49eecafc48095
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-02-14 17:36:04 +0100

    refactor

--- src/components/Fretboard/VariantProgressDots/parts.tsx
+++ src/components/Fretboard/VariantProgressDots/parts.tsx
@@ -9,31 +9,34 @@ export const DotsWrapper = styled.div`
   top: -5px;
   transform: translateX(-50%);
   z-index: 30;
+  background-color: var(--background);
+  padding: 2px 3px;
+  border-radius: 6px;
+  &:hover {
+    transform: scale(3);
+  }
 `;
 
 export const Dot = styled.div<{
   $isLearned: boolean;
   $isActive: boolean;
 }>`
-  width: 7px;
-  height: 9px;
-  border-radius: 3px;
+  width: 5px;
+  height: 5px;
+  border-radius: 6px;
   position: relative;
-  border: 1px solid color-mix(in oklab, var(--border) 80%, transparent);
   background-color: ${({ $isLearned }) => {
-    if ($isLearned) return "var(--accent)";
-    return "var(--background)";
-  }};
-  border-color: ${({ $isLearned }) => {
-    if ($isLearned) return "var(--background)";
+    if ($isLearned) return "var(--primary)";
     return "var(--accent)";
   }};
+
   filter: brightness(1.2);
   ${({ $isActive }) =>
     $isActive &&
     css`
-      transform: scale(1.3) translateY(-1px);
+      transform: scale(1.3);
       z-index: 1;
+      filter: brightness(2);
     `}
   will-change: transform, background-color;
   transition:
