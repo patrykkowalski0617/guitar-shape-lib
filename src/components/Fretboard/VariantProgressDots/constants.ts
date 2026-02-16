2927fa183789bf2683f888ce519518b667cd59b8
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-02-16 12:19:49 +0100

    invert shapes order

--- src/components/Fretboard/VariantProgressDots/constants.ts
+++ src/components/Fretboard/VariantProgressDots/constants.ts
@@ -1,7 +1,7 @@
 import { css } from "styled-components";
 
 export const activeDotsStyles = css`
-  transform: translate(-50%, 0%) scale(1.5);
+  transform: translate(-50%, 10%) scale(1.5);
   background: color-mix(in oklab, var(--background) 90%, transparent);
   box-shadow: 0 0 4px 4px var(--background);
   color: var(--background);
