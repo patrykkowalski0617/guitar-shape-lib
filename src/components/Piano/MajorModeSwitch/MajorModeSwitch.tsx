d7058633456d46700f58337968a48c0f8d7a8fe1
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-02-23 10:48:38 +0100

    add player - stage 14

--- src/components/Piano/MajorModeSwitch/MajorModeSwitch.tsx
+++ src/components/Piano/MajorModeSwitch/MajorModeSwitch.tsx
@@ -2,15 +2,22 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useControlsStore } from "@/store/useControlsStore";
 import * as S from "./parts";
+import { useMusicStore } from "@/store/useMusicStore";
 
 export function MajorModeSwitch() {
   const isMajorMode = useControlsStore((state) => state.isMajorMode);
   const setIsMajorMode = useControlsStore((state) => state.setIsMajorMode);
+  const setCurrentShapeVariantLocationData = useMusicStore((state) => state.setCurrentShapeVariantLocationData);
+
+  const handleValueChange = (checked: boolean) => {
+    setCurrentShapeVariantLocationData(null);
+    setIsMajorMode(checked);
+  };
 
   return (
     <S.ControlContainer>
       <div className="flex items-center space-x-2">
-        <Switch id="major-mode-switch" checked={isMajorMode} onCheckedChange={(checked) => setIsMajorMode(checked)} />
+        <Switch id="major-mode-switch" checked={isMajorMode} onCheckedChange={handleValueChange} />
         <Label htmlFor="major-mode-switch" className="text-muted-foreground font-light">
           {isMajorMode ? "Major Mode" : "Minor Mode"}
         </Label>
