9b79b0dce3b1f2503885e73e2e419a217ce19be8
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-02-23 17:58:46 +0100

    add player - stage 24 fix bpm input

---
+++ src/components/Player/PlayerControls/PlayButton.tsx
@@ -0,0 +1,23 @@
+import { Play, Square } from "lucide-react";
+import * as S from "./parts";
+import { usePlayerStore } from "@/store/usePlayerStore";
+
+export const PlayButton = () => {
+  const isPlaying = usePlayerStore((s) => s.isPlaying);
+  const isCountingIn = usePlayerStore((s) => s.isCountingIn);
+  const countIn = usePlayerStore((s) => s.countIn);
+  const togglePlay = usePlayerStore((s) => s.togglePlay);
+  const bpm = usePlayerStore((s) => s.bpm);
+
+  return (
+    <S.PlayButton $isPlaying={isPlaying} onClick={togglePlay} $bpm={bpm}>
+      {isCountingIn ? (
+        <span style={{ fontSize: "16px", fontWeight: "bold" }}>{countIn}</span>
+      ) : isPlaying ? (
+        <Square size={14} fill="currentColor" />
+      ) : (
+        <Play size={14} fill="currentColor" />
+      )}
+    </S.PlayButton>
+  );
+};
--- src/components/Player/PlayerControls/PlayerControls.tsx
+++ src/components/Player/PlayerControls/PlayerControls.tsx
@@ -1,30 +1,11 @@
-import { Play, Square } from "lucide-react";
-import * as S from "./parts";
-import { usePlayerStore } from "@/store/usePlayerStore";
+import { BpmInput } from "./BpmInput";
+import { PlayButton } from "./PlayButton";
 
-interface Props {
-  onBpmChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-export const PlayerControls = ({ onBpmChange }: Props) => {
-  const bpm = usePlayerStore((s) => s.bpm);
-  const isPlaying = usePlayerStore((s) => s.isPlaying);
-  const isCountingIn = usePlayerStore((s) => s.isCountingIn);
-  const countIn = usePlayerStore((s) => s.countIn);
-  const togglePlay = usePlayerStore((state) => state.togglePlay);
-
+export const PlayerControls = () => {
   return (
     <>
-      <S.PlayButton $isPlaying={isPlaying} onClick={togglePlay} $bpm={bpm}>
-        {isCountingIn ? (
-          <span style={{ fontSize: "16px", fontWeight: "bold" }}>{countIn}</span>
-        ) : isPlaying ? (
-          <Square size={14} fill="currentColor" />
-        ) : (
-          <Play size={14} fill="currentColor" />
-        )}
-      </S.PlayButton>
-      <S.BpmInput type="number" value={bpm} onChange={onBpmChange} min={20} max={360} />
+      <PlayButton />
+      <BpmInput />
     </>
   );
 };
