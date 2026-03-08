803d8bcdb6dfd4aaa374c9280e241f98c16cc135
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-03-08 18:16:18 +0100

    update text message

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,6 +1,5 @@
 export const USER_LIST_MESSAGES = {
-  SELECT_PROMPT:
-    "Please select a scale or arpeggio first and it`s shape on fretboard.",
+  SELECT_PROMPT: "Please select a shape first and it`s variant on fretboard.",
   ADDED: "Added to your collection.",
   REMOVED: "Removed from your collection.",
 };
