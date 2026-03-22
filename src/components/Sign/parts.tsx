030279fcaa3b4e493f4a877fdf7a5b2718bc664d
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-03-22 15:48:29 +0100

    Sing ui update

--- src/components/Sign/parts.tsx
+++ src/components/Sign/parts.tsx
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Sign = styled.header`
   font-weight: 100;
   font-style: italic;
-  color: var(--accent);
+  font-size: 18px;
+  color: color-mix(in oklab, var(--secondary) 80%, var(--secondary-foreground));
   text-align: right;
   width: 100%;
   padding: 5px 15px;
