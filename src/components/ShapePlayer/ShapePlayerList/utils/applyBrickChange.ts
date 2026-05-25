be4401ef610b7288135909137d534f5ffd0a2468
Patryk Kowalski <patrykkowalski0617@gmail.com>
2026-05-25 11:49:10 +0200

    fix restore target notes

--- src/components/ShapePlayer/ShapePlayerList/utils/applyBrickChange.ts
+++ src/components/ShapePlayer/ShapePlayerList/utils/applyBrickChange.ts
@@ -4,39 +4,6 @@ import { useMusicStore, useDataKeyStore } from "@/store";
 import type { ShapePlayerBrick } from "@/store";
 import { resolveTargetSharpNoteNames } from "@/utils";
 
-// export const applyBrickChange = (
-//   brick: ShapePlayerBrick,
-//   nextBrick: ShapePlayerBrick | undefined,
-// ) => {
-//   const { restoreCurrentBrick } = useDataKeyStore.getState();
-//   const { setBackingtrackNoteIds, replaceTargetSharpNoteNames } =
-//     useMusicStore.getState();
-
-//   restoreCurrentBrick({
-//     baseChordDataKey: brick.baseChordDataKey,
-//     unifiedMusicKeysDataKey: brick.unifiedMusicKeysDataKey,
-//     semitoneOffsetFromMajorRoot: brick.semitoneOffsetFromMajorRoot,
-//     selectedShapesVariantDataKeys: brickToSelectedShapes(brick),
-//   });
-
-//   const noteIds = brickToBackingtrackNoteIds(brick);
-//   if (noteIds.length > 0) {
-//     setBackingtrackNoteIds(noteIds);
-//   }
-
-//   if (nextBrick) {
-//     const sharpNoteNames = resolveTargetSharpNoteNames(
-//       nextBrick.unifiedMusicKeysDataKey,
-//       nextBrick.baseChordDataKey,
-//       nextBrick.guitarShapeDataKey,
-//       nextBrick.semitoneOffsetFromMajorRoot,
-//       nextBrick.targetNoteIndices ?? [1],
-//     );
-
-//     replaceTargetSharpNoteNames(sharpNoteNames);
-//   }
-// };
-
 export const applyBrickChange = (
   brick: ShapePlayerBrick,
   nextBrick: ShapePlayerBrick | undefined,
